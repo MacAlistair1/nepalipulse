537d20f89c2c61462de304c524a3e5d2d4c5fbe5
MacAlistair1 <lamichhaneaj@gmail.com>
2024-07-29 12:03:10 +0545

    top music video page

--- top-music-video.js
+++ top-music-video.js
@@ -6,12 +6,56 @@ fetch(`${baseUrl}/top_music_video.json`).then((response) => {
   rawData.then((data) => {
     let htmlData = "";
     data.forEach((item, index) => {
-      htmlData += `<tr>
-      <td>${item.bank}</td>
-      <td>${item.saving}</td>
-      <td>${item.fixed}</td>
-      </tr>`;
+      htmlData += `
+      <div class="music-item">
+        <img src="https://i3.ytimg.com/vi/${item.videoId}/hqdefault.jpg" alt="${
+        item.title
+      }" />
+        <div class="music-info">
+          <h3 title="${item.title}">${item.title}</h3>
+          <p>${formatNumber(item.views)} views</p>
+          <button onclick="window.location.href='http://www.youtube.com/watch?v=${
+            item.videoId
+          }'">Watch Video</button>
+        </div>
+      </div>
+      `;
+
+      if (index == 0) {
+        document.getElementById("last-updated").innerHTML = timestampToDate(
+          item.lastUpdate
+        );
+      }
     });
-    document.getElementById('bank-rate-list').innerHTML = htmlData;
+    document.getElementById("music-list").innerHTML = htmlData;
   });
 });
+
+const formatNumber = (value) => {
+  if (value >= 1e9) {
+    return (value / 1e9).toFixed(2) + "B +";
+  } else if (value >= 1e6) {
+    return (value / 1e6).toFixed(2) + "M +";
+  } else if (value >= 1e3) {
+    return (value / 1e3).toFixed(2) + "K +";
+  } else {
+    return value + " +";
+  }
+};
+
+const timestampToDate = (timestamp) => {
+  let date = new Date(timestamp);
+  return (
+    date.getDate() +
+    "/" +
+    (date.getMonth() + 1) +
+    "/" +
+    date.getFullYear() +
+    " " +
+    date.toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    })
+  );
+};
