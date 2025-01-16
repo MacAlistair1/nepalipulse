40d8a8b31324b29430fdfee7099172ab58fc1a9d
MacAlistair1 <lamichhaneaj@gmail.com>
2025-01-16 16:25:03 +0545

    quotes in social hashtags page

--- top-hashtags.js
+++ top-hashtags.js
@@ -4,20 +4,24 @@ document.addEventListener("DOMContentLoaded", () => {
   const keywordInput = document.getElementById("keyword");
   const searchButton = document.getElementById("searchButton");
   const hashtagsContainer = document.getElementById("hashtags");
+  const quotesContainer = document.getElementById("quotes");
   const loading = document.getElementById("loading");
 
   searchButton.addEventListener("click", () => {
     const keyword = keywordInput.value.trim();
     if (keyword.length > 0) {
-      loading.textContent = "fetching hashtags.";
+      loading.textContent = "fetching hashtags & quotes.";
       setInterval(() => {
         loading.textContent = loading.textContent + ".";
       }, 900);
       loading.style.display = "block";
       hashtagsContainer.innerHTML = "";
+      quotesContainer.innerHTML = "";
       fetchHashtags(keyword);
+      fetchQuotes(keyword);
     } else {
       hashtagsContainer.innerHTML = "";
+      quotesContainer.innerHTML = "";
     }
   });
 
@@ -25,6 +29,19 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch(`${baseUrl}/get-hashtags?keyword=${keyword}`)
       .then((response) => response.json())
       .then((data) => displayHashtags(data))
+      .catch((error) => {
+        loading.textContent = error;
+        setTimeout(() => {
+          loading.style.display = "none";
+        }, 1000);
+        console.error("Error fetching hashtags:", error);
+      });
+  };
+
+  const fetchQuotes = (keyword) => {
+    fetch(`${baseUrl}/get-quotes?keyword=${keyword}`)
+      .then((response) => response.json())
+      .then((data) => displayQuotes(data))
       .catch((error) => {
         loading.textContent = error;
         setTimeout(() => {
@@ -65,6 +82,36 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   };
 
+  const displayQuotes = (quotes) => {
+    
+    loading.style.display = "none";
+    quotesContainer.innerHTML = "";
+
+    if (quotes.length > 0) {
+      quotesContainer.innerHTML = `<hr><hr><br> <h3>Top Quotes</h3>`;
+    }
+
+    quotes.forEach((hashtagGroup) => {
+      const hashtagBox = document.createElement("div");
+      const hashtagElement = document.createElement("span");
+      hashtagElement.className = "hashtag";
+      hashtagElement.textContent = hashtagGroup;
+      hashtagBox.appendChild(hashtagElement);
+
+      const copyButton1 = document.createElement("button");
+      copyButton1.className = "copy-button";
+      copyButton1.textContent = "copy";
+      copyButton1.addEventListener("click", () => {
+        copyToClipboard(hashtagGroup, copyButton1);
+      });
+      hashtagBox.appendChild(copyButton1);
+
+      quotesContainer.appendChild(hashtagBox);
+      const brEl = document.createElement("br");
+      quotesContainer.appendChild(brEl);
+    });
+  };
+
   const copyToClipboard = (text, button) => {
     const textarea = document.createElement("textarea");
     textarea.value = text;
@@ -72,7 +119,7 @@ document.addEventListener("DOMContentLoaded", () => {
     textarea.select();
     document.execCommand("copy");
     document.body.removeChild(textarea);
-
+    
     // Change button text and color
     button.innerHTML = "copied";
     button.classList.add("copied");
